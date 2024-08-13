8e35d6fec44c524e230da5cba2a59864ee7ce0f8
David Kestering <davidkestering@gmail.com>
2024-08-13 14:06:29 +0000

    teste deploy na vercel

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e4a85d9a6bd8bf21"; }
+/******/ 		__webpack_require__.h = function() { return "5fd716d230ba5ad4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
